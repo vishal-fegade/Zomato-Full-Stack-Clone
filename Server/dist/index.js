48404de0a65d28d07ee05a8baa5b465c6ce6cd68
vishal-fegade <vsfegade.24@gmail.com>
2022-09-25 11:36:11 +0530

    completed setup of server & added babel to it, made mongo connection

---
+++ Server/dist/index.js
@@ -0,0 +1,32 @@
+"use strict";
+
+var _express = _interopRequireDefault(require("express"));
+
+var _dotenv = _interopRequireDefault(require("dotenv"));
+
+var _connection = _interopRequireDefault(require("./database/connection"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+// Database connection
+_dotenv.default.config();
+
+const zomato = (0, _express.default)();
+zomato.use(_express.default.json());
+zomato.get("/", (req, res) => {
+  res.json({
+    message: "Server is running"
+  });
+});
+const PORT = 4000;
+zomato.listen(PORT, () => {
+  // ConnectDB()
+  //   .then(() => {
+  //     console.log("Server is running !!!");
+  //   })
+  //   .catch((error) => {
+  //     console.log("Server is running, but database connection failed...");
+  //     console.log(error);
+  //   });
+  console.log("Server is running !!!");
+});
